3f6fd1c3a80977147c5299cf7559b50dac103cd7
Tanay Sinha <tanay.sinha@mail.utoronto.ca>
2023-07-21 16:55:54 +0530

    App.jsx, Navbar.jsx: Create and use Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
 
 export default function App() {
   return (
-    <h1>
-      Hello, World!
-    </h1>
+      <div className="w-screen h-screen">
+        <Navbar/>
+        </div>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav className="flex items-center text-4xl pl-6 h-20 bg-red-600 font-sans font-bold text-white	">
+      Pokédex
+    </nav>
+  );
+}
