ad26a2f262ed1d534e18050ff41360981b29be56
Tanay Sinha <tanay.sinha@mail.utoronto.ca>
2023-07-24 14:08:30 +0530

    Pokecard.jsx: Use SWR to make Pokecard get info from Pokeapi

--- src/components/Pokecard.jsx
+++ src/components/Pokecard.jsx
@@ -1,20 +1,41 @@
 import { useState } from 'react'
-import useSWR from 'swr'
+import useSWR, { SWRConfig } from 'swr'
 // import axios from 'axios'
 
+function localStorageProvider() {
+    // When initializing, we restore the data from `localStorage` into a map.
+    const map = new Map(JSON.parse(localStorage.getItem('app-cache') || '[]'))
+   
+    // Before unloading the app, we write back all the data into `localStorage`.
+    window.addEventListener('beforeunload', () => {
+      const appCache = JSON.stringify(Array.from(map.entries()))
+      localStorage.setItem('app-cache', appCache)
+    })
+   
+    // We still use the map for write & read for performance.
+    return map
+  }
 
 export default function Pokecard({sprite, name, number, gen}) {
     
-  
-    return (
-        <div className="flex flex-col items-center bg-slate-50 border border-grey-500">
-            <img className="mt-6 w-20"src={sprite}/>
-            <h2 className='text-3xl'>#{number}</h2>
-            <h1 className="text-3xl font-bold">{name}</h1>
-            <h2 className="text-xl">Generation {gen}</h2>
-        </div>
+    const fetcher = (url) => fetch(url).then(res => res.json())
+    const { data, error, isLoading } = useSWR(`https://pokeapi.co/api/v2/pokemon/${number}`, fetcher)
+    console.log(data)
+    console.log()
+    if (!isLoading && !error)
+    {
+        return (
+        <SWRConfig value={{ provider: localStorageProvider }}>
+            <div className="flex flex-col items-center bg-slate-50 border border-grey-500">
+                <img className="mt-6 w-20" src={data.sprites.other.dream_world.front_default}/>
+                <h2 className='text-3xl'>#{number}</h2>
+                <h1 className="capitalize text-3xl font-bold">{data.name}</h1>
+            </div>
+        </SWRConfig>
 
-    );
+        );
+    }
+
   }
 
   
