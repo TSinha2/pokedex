5fbbd75e9a2c76495d872d49c4acccc493b7a521
Tanay Sinha <tanay.sinha@mail.utoronto.ca>
2023-07-25 02:09:30 +0530

    App.jsx: Add crude version of infinite scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,57 @@
-import { useState } from 'react'
+import { useState, useCallback, useRef, useEffect } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import Pokecard from './components/Pokecard'
-import useSWR from 'swr'
+import { useBottomScrollListener } from 'react-bottom-scroll-listener';
 
 export default function App() {
+  let num = 21
   let pokemons = []
-  for (let i = 1; i < 21; i++)
+  for (let i = 1; i < num; i++)
   {
     pokemons.push(<Pokecard number={i}/>)
   }
+  const [p, setP] = useState(pokemons)
 
+  const observerTarget = useRef(null);
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      entries => {
+        if (entries[0].isIntersecting) {
+          // fetchData();
+          console.log("TEST")
+          let temp = []
+          for (let i = num; i < num+21; i++)
+          {
+            temp.push(<Pokecard number={i}/>)
+          }
+          setP([pokemons, temp]);
+          num += 21
+        }
+      },
+      { threshold: 1 }
+    );
+  
+    if (observerTarget.current) {
+      observer.observe(observerTarget.current);
+    }
+  
+    return () => {
+      if (observerTarget.current) {
+        observer.unobserve(observerTarget.current);
+      }
+    };
+  }, [observerTarget]);
+ 
   return (
-      <div className="w-screen h-screen">
+      <div  className="w-screen h-screen">
         <Navbar/>
-        <div className='grid grid-cols-2 gap-2 ml-2 mr-2'>
-          {pokemons}
+        <div  className='grid grid-cols-2 gap-2 ml-2 mr-2'>
+          {p}
         </div>
+        <div ref={observerTarget}></div>
+
       </div>
   )
+
 }
