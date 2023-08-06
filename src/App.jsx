c6c5b0e7a8184d68434dc59ae77e27fb0eadf6e9
Tanay Sinha <tanay.sinha@mail.utoronto.ca>
2023-08-06 13:54:29 +0530

    App.jsx: Implement router and use separated home page component

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,16 @@
-import { useState, useCallback, useRef, useEffect } from 'react'
-import './App.css'
-import Navbar from './components/Navbar'
-import Pokecard from './components/Pokecard'
+import './App.css';
+import PokeRoute from './components/PokeRoute.jsx';
+import Home from './components/Home';
+import { Route, Routes } from "react-router-dom"
+
 
 export default function App() {
-  let num = 21
-  let pokemons = []
-  for (let i = 1; i < num; i++)
-  {
-    pokemons.push(<Pokecard number={i}/>)
-  }
-  const [p, setP] = useState(pokemons)
-
-  const observerTarget = useRef(null);
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      entries => {
-        if (entries[0].isIntersecting) {
-          // fetchData();
-          console.log("TEST")
-          let temp = []
-          for (let i = num; i < num+21; i++)
-          {
-            temp.push(<Pokecard number={i}/>)
-          }
-          setP((prev) => [...prev, ...temp]);
-          num += 21
-        }
-      },
-      { threshold: 1 }
-    );
-  
-    if (observerTarget.current) {
-      observer.observe(observerTarget.current);
-    }
-  
-    return () => {
-      if (observerTarget.current) {
-        observer.unobserve(observerTarget.current);
-      }
-    };
-  }, [observerTarget]);
- 
   return (
       <div  className="w-screen h-screen">
-        <Navbar/>
-        <div  className='grid grid-cols-2 gap-2 ml-2 mr-2'>
-          {p}
-        </div>
-        <div ref={observerTarget}></div>
-
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pokemon/:id" element={<PokeRoute />} />
+        </Routes>
       </div>
   )
 
